956d831d703ab4732be911b58b3d375d2bb3ab56
Anton Johansson <anton.johansson@monterosa.se>
2015-01-21 23:25:26 +0100

    Current column rewrite

--- highlight-indentation.el
+++ highlight-indentation.el
@@ -77,9 +77,8 @@
   "Erease and read overlays between START and END."
   (save-match-data
     (save-excursion
-      (let ((inhibit-point-motion-hooks t))
-        (goto-char end)
-        (setq end (line-beginning-position 2))
+      (let ((inhibit-point-motion-hooks t)
+            (end (save-excursion (goto-char end) (line-beginning-position 2))))
         (highlight-indentation-delete-overlays-region start end overlay)
         (funcall func start end overlay)))))
 
@@ -104,10 +103,44 @@
                 last-indent pos
                 o (make-overlay pos (+ pos 1)))
           (overlay-put o overlay t)
+          (overlay-put o 'priority highlight-indentation-overlay-priority)
           (overlay-put o 'face 'highlight-indentation-face))
         (forward-char))
       (forward-line) ;; Next line
       (setq pos (point)))))
+
+(defun highlight-indentation-guess-offset ()
+  "Get indentation offset of current buffer"
+  (cond ((and (eq major-mode 'python-mode) (boundp 'python-indent))
+         python-indent)
+        ((and (eq major-mode 'python-mode) (boundp 'py-indent-offset))
+         py-indent-offset)
+        ((and (eq major-mode 'python-mode) (boundp 'python-indent-offset))
+         python-indent-offset)
+        ((eq major-mode 'ruby-mode)
+         ruby-indent-level)
+        ((and (eq major-mode 'scala-mode) (boundp 'scala-indent:step))
+         scala-indent:step)
+        ((and (eq major-mode 'scala-mode) (boundp 'scala-mode-indent:step))
+         scala-mode-indent:step)
+        ((or (eq major-mode 'scss-mode) (eq major-mode 'css-mode))
+         css-indent-offset)
+        ((eq major-mode 'nxml-mode)
+         nxml-child-indent)
+        ((eq major-mode 'coffee-mode)
+         coffee-tab-width)
+        ((eq major-mode 'js-mode)
+         js-indent-level)
+        ((eq major-mode 'js2-mode)
+         js2-basic-offset)
+        ((and (fboundp 'derived-mode-class) (eq (derived-mode-class major-mode) 'sws-mode))
+         sws-tab-width)
+        ((eq major-mode 'web-mode)
+         web-mode-html-offset) ; other similar vars: web-mode-{css-indent,scripts}-offset
+        ((local-variable-p 'c-basic-offset)
+         c-basic-offset)
+        (t
+         (default-value 'highlight-indentation-offset))))
 
 ;;;###autoload
 (define-minor-mode highlight-indentation-mode
@@ -121,39 +154,8 @@
   (when highlight-indentation-mode ;; ON
     (when (not (local-variable-p 'highlight-indentation-offset))
       (set (make-local-variable 'highlight-indentation-offset)
-           ;; Set indentation offset from highlight-indentation-offset if set, otherwise
-           ;; according to major mode
-           (cond ((and (eq major-mode 'python-mode) (boundp 'python-indent))
-                  python-indent)
-                 ((and (eq major-mode 'python-mode) (boundp 'py-indent-offset))
-                  py-indent-offset)
-                 ((and (eq major-mode 'python-mode) (boundp 'python-indent-offset))
-                  python-indent-offset)
-                 ((eq major-mode 'ruby-mode)
-                  ruby-indent-level)
-                 ((and (eq major-mode 'scala-mode) (boundp 'scala-indent:step))
-                  scala-indent:step)
-                 ((and (eq major-mode 'scala-mode) (boundp 'scala-mode-indent:step))
-                  scala-mode-indent:step)
-                 ((or (eq major-mode 'scss-mode) (eq major-mode 'css-mode))
-                  css-indent-offset)
-                 ((eq major-mode 'nxml-mode)
-                  nxml-child-indent)
-                 ((eq major-mode 'coffee-mode)
-                  coffee-tab-width)
-                 ((eq major-mode 'js-mode)
-                  js-indent-level)
-                 ((eq major-mode 'js2-mode)
-                  js2-basic-offset)
-                 ((and (fboundp 'derived-mode-class) (eq (derived-mode-class major-mode) 'sws-mode))
-                  sws-tab-width)
-                 ((eq major-mode 'web-mode)
-                  web-mode-html-offset) ; other similar vars: web-mode-{css-indent,scripts}-offset
-                 ((local-variable-p 'c-basic-offset)
-                  c-basic-offset)
-                 (t
-                  (default-value 'highlight-indentation-offset)))))
-
+           (highlight-indentation-guess-offset)))
+    
     ;; Setup hooks
     (dolist (hook highlight-indentation-hooks)
       (apply 'add-hook hook))
@@ -173,40 +175,89 @@ from major mode"
   (when highlight-indentation-mode
     (highlight-indentation-mode)))
 
-
-;;;
-;;; Copyright (C) Kresten Krab Thorup
-;;; Available under Apache License, Version 2.
-;;;
 ;;; This minor mode will highlight the indentation of the current line
 ;;; as a vertical bar (grey background color) aligned with the column of the
 ;;; first character of the current line.
-;;;
 (defface highlight-indentation-current-column-face
   ;; Fringe has non intrusive color in most color-themes
-  '((t :inherit fringe))
+  '((t (:background "black")))
   "Basic face for highlighting indentation guides."
   :group 'highlight-indentation)
 
-;; used to hold the last regex we installed
-(defvar highlight-indentation-current-column-regex nil)
+(defvar highlight-indentation-overlay-priority 1)
+(defvar highlight-indentation-current-column-overlay-priority 2)
+
+(defconst highlight-indentation-current-column-hooks
+  '((after-change-functions (lambda (start end length)
+                              (highlight-indentation-redraw-region
+                               start end
+                               'highlight-indentation-current-column-overlay
+                               'highlight-indentation-current-column-put-overlays-region))
+                            t t)
+    (post-command-hook (lambda () 
+                         (highlight-indentation-redraw-all-windows 'highlight-indentation-current-column-overlay
+                                                                   'highlight-indentation-current-column-put-overlays-region)
+                         ))
+    (window-scroll-functions (lambda (win start)
+                               (highlight-indentation-redraw-window
+                                win
+                                'highlight-indentation-current-column-overlay
+                                'highlight-indentation-current-column-put-overlays-region
+                                start))
+                             nil t)))
+
+(defun highlight-indentation-current-column-put-overlays-region (start end overlay)
+  "Place overlays between START and END."
+  (let (o ;; overlay
+        (last-indent 0)
+        (indent (save-excursion (back-to-indentation) (current-column)))
+        (pos start))
+    (goto-char start)
+    ;; (message "doing it %d" indent)
+    (while (< pos end)
+      (beginning-of-line)
+      (while (and (integerp (char-after))
+                  (not (= 10 (char-after))) ;; newline
+                  (= 32 (char-after))) ;; space
+        (when (= (current-column) indent)
+          (setq pos (point)
+                last-indent pos
+                o (make-overlay pos (+ pos 1)))
+          (overlay-put o overlay t)
+          (overlay-put o 'priority highlight-indentation-current-column-overlay-priority)
+          (overlay-put o 'face 'highlight-indentation-current-column-face))
+        (forward-char))
+      (forward-line) ;; Next line
+      (setq pos (point)))))
 
 ;;;###autoload
-(define-minor-mode
-  highlight-indentation-current-column-mode
-  "Hilight Indentation minor mode displays
-a vertical bar corresponding to the indentation of the current line"
+(define-minor-mode highlight-indentation-current-column-mode
+  "Hilight Indentation minor mode displays a vertical bar
+corresponding to the indentation of the current line"
   :lighter " |"
+  
+  (when (not highlight-indentation-current-column-mode) ;; OFF
+    (highlight-indentation-delete-overlays-buffer 'highlight-indentation-current-column-overlay)
+    (dolist (hook highlight-indentation-current-column-hooks)
+      (remove-hook (car hook) (nth 1 hook) (nth 3 hook))))
 
-  (when highlight-indentation-current-column-regex
-    (font-lock-remove-keywords nil highlight-indentation-current-column-regex))
-
-  (set (make-local-variable 'highlight-indentation-current-column-regex) nil)
-  (cond  (highlight-indentation-current-column-mode
-          (add-hook 'post-command-hook 'highlight-indentation-current-column-post-command-hook nil t))
-         (t
-          (remove-hook 'post-command-hook 'highlight-indentation-current-column-post-command-hook t)
-          (font-lock-fontify-buffer))))
+  (when highlight-indentation-current-column-mode ;; ON
+    (when (not (local-variable-p 'highlight-indentation-offset))
+      (set (make-local-variable 'highlight-indentation-offset)
+           (highlight-indentation-guess-offset)))
+    
+    ;; Setup hooks
+    (dolist (hook highlight-indentation-current-column-hooks)
+      (apply 'add-hook hook))
+    (highlight-indentation-redraw-all-windows 'highlight-indentation-current-column-overlay
+                                              'highlight-indentation-current-column-put-overlays-region))
+  
+  ;; (cond  (highlight-indentation-current-column-mode
+  ;;         (add-hook 'post-command-hook 'highlight-indentation-current-column-post-command-hook nil t))
+  ;;        (t
+  ;;         (remove-hook 'post-command-hook 'highlight-indentation-current-column-post-command-hook t)
+  ;;         (font-lock-fontify-buffer)))
+)
 
 (defun highlight-indentation-current-column-post-command-hook ()
   "This hook runs after every keystroke"
